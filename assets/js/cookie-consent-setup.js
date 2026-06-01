0f1e9683d66c6779a33fd2c0eedf42851c60af33
thucltt <hoangtukute1320@gmail.com>
2026-06-01 20:22:56 +0000

    Deploying to gh-pages from @ thucltt/thucltt.github.io@6c899d6483b0a72625a3ca2d36c5acbe2ed0feca 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://thucltt.com/demo_new_site/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://thucltt.com/#contact">contact us</a>.'
               }
             ]
           }
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,21 +7,21 @@ ninja.data = [{
     title: "About",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/demo_new_site/";
+      window.location.href = "/";
     },
   },{id: "nav-blog",
           title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/demo_new_site/blog/";
+            window.location.href = "/blog/";
           },
         },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/demo_new_site/cv/";
+            window.location.href = "/cv/";
           },
         },{id: "post-talking-with-students-at-the-nguyen-tat-thanh-institute-of-international-education",
         
@@ -31,7 +31,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/demo_new_site/blog/2023/talking-with-students-at-niie-2023/";
+            window.location.href = "/blog/2023/talking-with-students-at-niie-2023/";
           
         },
       },{id: "post-using-blockchain-in-retrieval-system-student-accomplishments",
@@ -42,7 +42,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/demo_new_site/blog/2022/using-blockchain-in-retrieval-system-student-accomplishments/";
+            window.location.href = "/blog/2022/using-blockchain-in-retrieval-system-student-accomplishments/";
           
         },
       },{id: "post-coder-in-prevention-covid-19",
@@ -53,7 +53,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/demo_new_site/blog/2022/coder-in-prevention-covid-19/";
+            window.location.href = "/blog/2022/coder-in-prevention-covid-19/";
           
         },
       },{id: "post-thinking-about-metaverse-from-a-newbie-39-s-perspective",
@@ -64,7 +64,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/demo_new_site/blog/2021/thinking-about-metaverse/";
+            window.location.href = "/blog/2021/thinking-about-metaverse/";
           
         },
       },{id: "post-thank-you",
@@ -75,7 +75,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/demo_new_site/blog/2021/cantho-2020-code/";
+            window.location.href = "/blog/2021/cantho-2020-code/";
           
         },
       },{id: "post-bài-viết-đầu-tiên-của-tôi",
@@ -86,14 +86,14 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/demo_new_site/blog/2020/first-post/";
+            window.location.href = "/blog/2020/first-post/";
           
         },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/demo_new_site/books/the_godfather/";
+              window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -101,7 +101,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/demo_new_site/news/announcement_2/";
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -109,57 +109,57 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/1_project/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/2_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/3_project/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/4_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/5_project/";
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/6_project/";
+              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/7_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/8_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/demo_new_site/projects/9_project/";
+              window.location.href = "/projects/9_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
-              window.location.href = "/demo_new_site/teachings/data-science-fundamentals/";
+              window.location.href = "/teachings/data-science-fundamentals/";
             },},{id: "teachings-introduction-to-machine-learning",
           title: 'Introduction to Machine Learning',
           description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
           section: "Teachings",handler: () => {
-              window.location.href = "/demo_new_site/teachings/introduction-to-machine-learning/";
+              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -193,7 +193,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/demo_new_site/feed.xml", "_blank");
+          window.open("/feed.xml", "_blank");
         },
       },{
       id: 'light-theme',
